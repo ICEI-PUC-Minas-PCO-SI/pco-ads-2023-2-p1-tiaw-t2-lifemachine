6871679d2fe12a46066476d15285cf260ec0766c
Daniel <danieltelesdp@gmail.com>
2023-11-19 09:31:46 -0300

    Modelei a solução errada, terei que refazer

--- RF-011/script.js
+++ RF-011/script.js
@@ -1,88 +1,90 @@
-function myFunction1(){
-    var numero1 = document.querySelector("numero1").value;
-    console.log(numero1);
-}
-
-/*
 function myFunction1() {
-    var numero1 = parseInt(document.getElementById("numero1").value);
-    return numero1;
+    let numero1 = document.getElementById("numero1");
+    return numero1.value;
 }
 
 function myFunction2() {
-    var numero2 = parseInt(document.getElementById("numero2").value);
-    return numero2;
+    let numero2 = document.getElementById("numero2");
+    return numero2.value;
 }
 
-let horaMinuto = new Date();
-horaMinuto.setHours(myFunction1());
-horaMinuto.setMinutes(myFunction2());
+function criarHora(){
+    let Horario = {
+        Hora: myFunction1(),
+        Minuto: myFunction2(),
+    }
+    return Horario;
+}
 
 
 function modelarHoras (horas, tempoCiclo){
     if(horas < 9){
-        let resultado = 24 - (tempoCiclo - horas);
-        return  resultado;
+        var resultado1 = 24 - (tempoCiclo - horas);
+        return  resultado1;
     }else{
-        let resultado = horas - tempoCiclo;
-        return resultado;
+        var resultado1 = horas - tempoCiclo;
+        return resultado1;
     }
 }
 function modelarMinutos (minutos, tempoCiclo){
-    if(horas < 9){
-        let resultado = 60 - (tempoCiclo - minutos);
-        return  resultado;
+    if(minutos < 9){
+        var resultado2 = 60 - (tempoCiclo - minutos);
+        return  resultado2;
     }else{
-        let resultado = minutos - tempoCiclo;
-        return resultado;
+        var resultado2 = minutos - tempoCiclo;
+        return resultado2;
     }
 }
 
-var ciclo1 = {
-    horaNum: modelarHoras(horaMinuto.getHours(), 9),
-    MinNum: modelarHoras(horaMinuto.getMinutes(), 9),
-    horaString: modelarMinutos(horaMinuto.getHours(), 0).toString(),
-    MinString: modelarMinutos(horaMinuto.getMinutes(), 0).toString(),
-};
-
-var ciclo2 = {
-    horaNum: modelarHoras(horaMinuto.getHours(), 8),
-    MinNum: modelarHoras(horaMinuto.getMinutes(), 8),
-    horaString: modelarMinutos(horaMinuto.getHours(), 30).toString(),
-    MinString: modelarMinutos(horaMinuto.getMinutes(), 30).toString(),
-};
-
-var ciclo3 = {
-    horaNum: modelarHoras(horaMinuto.getHours(), 7),
-    MinNum: modelarHoras(horaMinuto.getMinutes(), 7),
-    horaString: modelarMinutos(horaMinuto.getHours(), 30).toString(),
-    MinString: modelarMinutos(horaMinuto.getMinutes(), 30).toString(),
-};
-
-var ciclo4 = {
-    horaNum: modelarHoras(horaMinuto.getHours(), 6),
-    MinNum: modelarHoras(horaMinuto.getMinutes(), 6),
-    horaString: modelarMinutos(horaMinuto.getHours(), 30).toString(),
-    MinString: modelarMinutos(horaMinuto.getMinutes(), 30).toString(),
-};
-
-var ciclo5 = {
-    horaNum: modelarHoras(horaMinuto.getHours(), 6),
-    MinNum: modelarHoras(horaMinuto.getMinutes(), 6),
-    horaString: modelarMinutos(horaMinuto.getHours(), 30).toString(),
-    MinString: modelarMinutos(horaMinuto.getMinutes(), 30).toString(),
-};
-
-
-var ciclo6 = {
-    horaNum: modelarHoras(horaMinuto.getHours(), 5),
-    MinNum: modelarHoras(horaMinuto.getMinutes(), 5),
-    horaString: modelarMinutos(horaMinuto.getHours(), 30).toString(),
-    MinString: modelarMinutos(horaMinuto.getMinutes(), 30).toString(),
-};
 
 
 function teste() {
+
+    var horaMinuto = criarHora();
+
+    var ciclo1 = {
+        horaNum: modelarHoras(criarHora().Hora, 9),
+        MinNum: modelarHoras(horaMinuto.Hora, 9),
+        horaString: modelarMinutos(criarHora().Hora, 9).toString(),
+        MinString: modelarMinutos(criarHora().Minuto, 0).toString(),
+    };
+    
+    var ciclo2 = {
+        horaNum: modelarHoras(horaMinuto.Horas, 8),
+        MinNum: modelarHoras(horaMinuto.Horas, 8),
+        horaString: modelarMinutos(criarHora().Hora, 8).toString(),
+        MinString: modelarMinutos(criarHora().Minuto, 30).toString(),
+    };
+    
+    var ciclo3 = {
+        horaNum: modelarHoras(horaMinuto.Horas, 7),
+        MinNum: modelarHoras(horaMinuto.Horas, 7),
+        horaString: modelarMinutos(criarHora().Hora, 7).toString(),
+        MinString: modelarMinutos(criarHora().Minuto, 30).toString(),
+    };
+    
+    var ciclo4 = {
+        horaNum: modelarHoras(horaMinuto.Horas, 6),
+        MinNum: modelarHoras(horaMinuto.Horas, 6),
+        horaString: modelarMinutos(criarHora().Hora, 6).toString(),
+        MinString: modelarMinutos(criarHora().Minuto, 30).toString(),
+    };
+    
+    var ciclo5 = {
+        horaNum: modelarHoras(horaMinuto.Horas, 5),
+        MinNum: modelarHoras(horaMinuto.Horas, 5),
+        horaString: modelarMinutos(criarHora().Hora, 5).toString(),
+        MinString: modelarMinutos(criarHora().Minuto, 30).toString(),
+    };
+    
+    
+    var ciclo6 = {
+        horaNum: modelarHoras(horaMinuto.Horas, 4),
+        MinNum: modelarHoras(horaMinuto.Horas, 4),
+        horaString: modelarMinutos(criarHora().Hora, 4).toString(),
+        MinString: modelarMinutos(criarHora().Minuto, 30).toString(),
+    };
+
     let Divtela = document.getElementById("x");
     let newDiv = `
             <table class="table table-striped">
@@ -134,15 +136,70 @@ function teste() {
             </table>
         `;
 
-    Divtela.innerHTML(newDiv);
+    Divtela.innerHTML = newDiv;
 }
-
 
 
 
     
 
 
+/* //Função de teste apenas
+    function teste() {
+        let Divtela = document.getElementById("x");
+        let newDiv = `
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Ciclo</th>
+                        <th scope="col">Hora de adormecer</th>
+                        <th scope="col">Tempo de Sono</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>1</td>
+                        <td>ewrwerer:34234342</td>
+                        <td>99999999999999</td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>2</td>
+                        <td>ewrwerer:34234342</td>
+                        <td>450</td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>3</td>
+                        <td>ewrwerer:34234342</td>
+                        <td>360</td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>4</td>
+                        <td>ewrwerer:34234342</td>
+                        <td>360</td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>5</td>
+                        <td>ewrwerer:34234342</td>
+                        <td>360</td>
+                    </tr>
+                    <tr>
+                    <th scope="row"></th>
+                        <td>6</td>
+                        <td>ewrwerer:34234342</td>
+                        <td>360</td>
+                    </tr>
+                </tbody>
+            </table>
+        `;
+
+    Divtela.innerHTML = newDiv;
+}
+*/
 
 
 /*
