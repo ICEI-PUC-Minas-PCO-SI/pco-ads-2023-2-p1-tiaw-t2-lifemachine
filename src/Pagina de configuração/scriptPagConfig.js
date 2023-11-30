732dd3fc278a1bbfd913becc27e3f53100953d36
Daniel <danieltelesdp@gmail.com>
2023-11-29 22:42:32 -0300

    Testes práticos json, falta montar estrutura

--- src/Pagina de configuração/scriptPagConfig.js
+++ src/Pagina de configuração/scriptPagConfig.js
@@ -1,11 +1,11 @@
 
 function teste2(){
-const idUsuario = "1";
+const idUsuario = "2";
 const novoPeso = "234";
 
 // Dados a serem enviados na requisição PUT
 const dadosAtualizados = {
-  teste: "teste"
+  testertd: "teste"
 };
 
 // Configuração da requisição
@@ -17,11 +17,29 @@ const opcoes = {
   body: JSON.stringify(dadosAtualizados)
 };
 
+const opcoes2 = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(dadosAtualizados)
+};
+
+const opcoes3 = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+
 // URL do servidor JSON (substitua pelo seu endpoint correto)
-const url = `http://localhost:3000/usuarios/${idUsuario}`;
+const url1 = `http://localhost:3000/usuarios/${idUsuario}`;
+const url2 = `http://localhost:3000/daniel/`;
+const url3 = `http://localhost:3000/daniel/id`;
+
 
 // Envia a requisição
-fetch(url, opcoes)
+fetch(url1, opcoes3)
   .then(response => {
     if (!response.ok) {
       throw new Error('Erro ao tentar atualizar o peso do usuário');
@@ -34,4 +52,26 @@ fetch(url, opcoes)
   .catch(error => {
     console.error('Erro:', error.message);
   });
+  alert(ok)
 }
+
+fetch(url1, opcoes3)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro ao tentar obter o peso do usuário');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Aqui, você pode armazenar o valor obtido em uma variável
+    const pesoDoUsuario = data.peso; // Certifique-se de adaptar conforme a estrutura da resposta
+    console.log('Peso do usuário obtido com sucesso:', pesoDoUsuario);
+
+    // Agora você pode usar a variável "pesoDoUsuario" conforme necessário
+    // Por exemplo, você pode exibir um alerta com o valor:
+    alert(`Peso do usuário: ${pesoDoUsuario}`);
+  })
+  .catch(error => {
+    console.error('Erro:', error.message);
+  });
+
