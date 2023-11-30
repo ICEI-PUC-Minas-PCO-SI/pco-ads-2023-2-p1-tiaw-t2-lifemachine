bead083a700f0a12ca487af34cd19a0ca97f9d09
Daniel <danieltelesdp@gmail.com>
2023-11-29 23:11:49 -0300

    Testes com metodo GET funcionaram, mais testes

--- src/Pagina de configuração/scriptPagConfig.js
+++ src/Pagina de configuração/scriptPagConfig.js
@@ -3,6 +3,7 @@ function teste2(){
 const idUsuario = "2";
 const novoPeso = "234";
 
+
 // Dados a serem enviados na requisição PUT
 const dadosAtualizados = {
   testertd: "teste"
@@ -55,7 +56,23 @@ fetch(url1, opcoes3)
   alert(ok)
 }
 
-fetch(url1, opcoes3)
+function teste3(){
+const idUsuario = "2";
+const novoPeso = "234";
+
+const opcoes3 = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+
+// URL do servidor JSON (substitua pelo seu endpoint correto)
+const url1 = `http://localhost:3000/usuarios/${idUsuario}`;
+const url2 = `http://localhost:3000/daniel/`;
+const url3 = `http://localhost:3000/daniel/`;
+
+fetch(url3, opcoes3)
   .then(response => {
     if (!response.ok) {
       throw new Error('Erro ao tentar obter o peso do usuário');
@@ -64,7 +81,7 @@ fetch(url1, opcoes3)
   })
   .then(data => {
     // Aqui, você pode armazenar o valor obtido em uma variável
-    const pesoDoUsuario = data.peso; // Certifique-se de adaptar conforme a estrutura da resposta
+    var pesoDoUsuario = data[0].id; // Certifique-se de adaptar conforme a estrutura da resposta
     console.log('Peso do usuário obtido com sucesso:', pesoDoUsuario);
 
     // Agora você pode usar a variável "pesoDoUsuario" conforme necessário
@@ -75,3 +92,5 @@ fetch(url1, opcoes3)
     console.error('Erro:', error.message);
   });
 
+
+}
