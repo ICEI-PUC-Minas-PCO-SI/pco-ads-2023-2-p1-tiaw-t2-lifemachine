59fb39b47f21ce59da5507a227948e0f42738930
Pedro Ferraz <pepito.ferraz@gmail.com>
2023-12-02 19:53:47 -0300

    Conclusao RF-014 - exibir estatisticas

--- src/Página de treino/treino.js
+++ src/Página de treino/treino.js
@@ -1,112 +1,108 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const URL = 'http://localhost:3000/usuarios';
-    //Exibe o peso atual do usuario na box do meio
-    const pesoAtualArea = document.getElementById("peso-atual")
-    var peso = JSON.parse(localStorage.getItem("pesoAtual"))
+//Exibe o peso atual do usuario na box do meio
+const pesoAtualArea = document.getElementById("peso-atual")
+var peso = JSON.parse(localStorage.getItem("pesoAtual"))
 
-    pesoAtualArea.innerHTML = `${peso}Kg`
+pesoAtualArea.innerHTML = `${peso}Kg`
 
-    //=============================================================================================================
-    
-    //ao clicar no botao adicionar um treino a mais, no mes atual, porem so pode clicar uma vez ao dia
-    //atualizar o grafico
-    //guardar dados no JSON server
+//=============================================================================================================
 
-    var dataAtual = new Date();
-    var mes = dataAtual.getMonth();
-    var dia = dataAtual.getDate();
-    var jsonMes = `QtdTreinoMes` + mes
-    var treinoConcluidos = 0;
-    //Adicionar treinos concluidos
-    
-    var botao_treino = document.getElementById('concluir-treino')
-    botao_treino.addEventListener('click',function(event){
+//ao clicar no botao adicionar um treino a mais, no mes atual, porem so pode clicar uma vez ao dia
+//atualizar o grafico
+//guardar dados no JSON server
 
-        const auxClique = dia;
-        const ultimoClique = localStorage.getItem('ultimoClique')
-
-        if(ultimoClique == auxClique) {
-            event.preventDefault()
-        }
-        else(
-            treinoConcluidos++
-        )
-
-        localStorage.setItem('ultimoClique',auxClique)
-    })
+var dataAtual = new Date();
+var mes = dataAtual.getMonth();
+var dia = dataAtual.getDate();
+var jsonMes = `QtdTreinoMes` + mes
+var treinoConcluidos = 0;
 
 
-    var auxID = JSON.parse(localStorage.getItem('atualID'))
+const URL = 'http://localhost:3000/usuarios';
+var auxID = JSON.parse(localStorage.getItem('atualID'))
 
+//Adicionar treinos concluidos
+var botao_treino = document.getElementById('concluir-treino')
+botao_treino.addEventListener('click', function (event) {
+    const auxClique = dia;
+    const ultimoClique = localStorage.getItem('ultimoClique')
 
-    fetch(`${URL}/${auxID}`)
+    if (ultimoClique != auxClique) {
+        treinoConcluidos++
+        localStorage.setItem('ultimoClique', auxClique)
+
+        // Atualiza o JSON Server
+        fetch(`${URL}/${auxID}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ [jsonMes]: treinoConcluidos }), // Atualiza apenas o mês atual
+        })
         .then(response => {
-            if (response.ok) {
-                // Se o usuário existir, realizar o PATCH apenas se a estrutura do mês não existir
-                return fetch(`${URL}/${auxID}`)
-                    .then(res => res.json())
-                    .then(usuario => {
-                        if (!usuario[jsonMes]) {
-                            return fetch(`${URL}/${auxID}`, {
-                                method: "PATCH",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify({ [jsonMes]: auxPeso }), // Atualiza apenas o mês atual
-                            });
-                        }
-                    });
-            } else {
-                console.error("Usuário não encontrado.");
+            if (!response.ok) {
+                throw new Error("Falha ao atualizar os treinos.");
             }
         })
         .catch(error => {
             console.error("Erro durante a operação:", error);
+        })
+        .finally(() => {
+            // Após atualizar, atualiza o gráfico
+            atualizarGrafico();
         });
+    } else {
+        event.preventDefault()
+    }
+});
 
-
+// Função para atualizar o gráfico
+function atualizarGrafico() {
     fetch(`${URL}/${auxID}`)
         .then(res => res.json())
         .then(users => {
-            
+            const diasTreino = [];
+
+            for (let i = 0; i < 12; i++) {
+                const aux = `QtdTreinoMes${i}`;
+
+                diasTreino[i] = users[aux] || 0;
+            }
+
             var dadosDoGrafico = {
                 labels: ['Novembro', 'Dezembro', 'Janeiro', 'Fevereiro'],
                 datasets: [{
-                    label: 'Treinos concluidos',
-                    data: [6, 3, 2, 1],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.5)',
-                        'rgba(255, 99, 132, 0.5)',
-                        'rgba(255, 99, 132, 0.5)',
-                        'rgba(255, 99, 132, 0.5)',
+                    label: 'Treinos concluídos',
+                    data: [
+                        diasTreino[10],
+                        diasTreino[11],
+                        diasTreino[0],
+                        diasTreino[1]
                     ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 99, 132, 1)',
-                    ],
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
                     borderWidth: 1
                 }]
             };
-    
-            // Configuração do gráfico
-            var config = {
+
+            var ctx = document.getElementById('myBarChart').getContext('2d');
+            var myBarChart = new Chart(ctx, {
                 type: 'bar',
                 data: dadosDoGrafico,
                 options: {
                     scales: {
                         y: {
-                            beginAtZero: true
+                            beginAtZero: true,
+                            suggestedMin: 0, // Força o valor mínimo a ser zero
+                            stepSize: 1 
                         }
                     }
                 }
-            };
-    
-            // Criação do gráfico
-            var ctx = document.getElementById('myBarChart').getContext('2d');
-            var myBarChart = new Chart(ctx, config);
+            });
+        })
+        .catch(error => {
+            console.error("Erro durante a operação:", error);
         });
+}
 
- })
+atualizarGrafico();
 
