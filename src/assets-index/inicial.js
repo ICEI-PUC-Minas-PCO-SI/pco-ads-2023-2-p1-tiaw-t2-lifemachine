8607786f0e3f5daa3a2c5762eb39bee87199d24b
Pedro Ferraz <pepito.ferraz@gmail.com>
2023-12-14 11:33:32 -0300

    correçao de url

--- src/assets-index/inicial.js
+++ src/assets-index/inicial.js
@@ -8,7 +8,7 @@ let max = 10;
 let numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
 
 // Puxa dados de uma biblioteca local JSON
-fetch('mensagens-motivacionais.json')
+fetch('assets-index/mensagens-motivacionais.json')
   .then(response => response.json())
   .then(data => {
     dbMensagens = data;
