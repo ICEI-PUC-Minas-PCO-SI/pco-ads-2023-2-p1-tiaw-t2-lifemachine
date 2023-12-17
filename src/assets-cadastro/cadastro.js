6c9bc8779128b89b3c66d2dffdaac2de4680e1ee
Caio <Caiof113araujo@gmail.com>
2023-12-17 20:30:29 -0300

    progresso

--- src/assets-cadastro/cadastro.js
+++ src/assets-cadastro/cadastro.js
@@ -86,19 +86,22 @@ $(document).ready(function () {
             });
         });
 
-    function validarCampos() {
-        var camposObrigatorios = ['#email', '#password', '#1password', '#nome', '#Altura', '#Peso', '#Objetivo', '#DiasDeTreino', '#Sexo'];
-        var camposFaltando = [];
-
-        camposObrigatorios.forEach(function(campo) {
-            var valorCampo = $(campo).val();
-            if (!valorCampo || valorCampo.trim() === '') {
-                camposFaltando.push(campo);
-            }
-        });
-
-        return camposFaltando;
-    }
+        function validarCampos() {
+            var camposObrigatorios = ['#email', '#password', '#1password', '#nome', '#Altura', '#Peso', '#Objetivo', '#DiasDeTreino', '#Sexo'];
+            var camposFaltando = [];
+        
+            camposObrigatorios.forEach(function(campo) {
+                var valorCampo = $(campo).val();
+                if (!valorCampo || valorCampo.trim() === '') {
+                    camposFaltando.push(campo);
+                    $(campo).addClass('focus-highlight'); 
+                } else {
+                    $(campo).removeClass('focus-highlight'); 
+                }
+            });
+        
+            return camposFaltando;
+        }
 
     function emailUsado(email) {
         for (var i = 0; i < users.length; i++) {
@@ -134,13 +137,10 @@ $(document).ready(function () {
         }
     }
     $('input, select').on('focus', function () {
-        console.log('Campo em foco:', this);
         $(this).addClass('focus-highlight');
     });
 
     $('input, select').on('blur', function () {
-        console.log('Campo fora de foco:', this);
-
         if ($(this).val().trim() === '') {
             $(this).addClass('focus-highlight');
         } else {
